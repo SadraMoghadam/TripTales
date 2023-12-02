ac15f01d91391a6c7a4ae3565cb4bfdc7d05fdeb
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2023-12-02 13:59:03 +0100

    added custom text field

---
+++ lib/src/widgets/text_field.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final String? hintText;
+  final IconData prefixIcon;
+  final IconData? suffixIcon;
+  final bool obscureText;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final bool enabled;
+  final TextInputType keyboardType;
+  final TextInputAction? textInputAction;
+  final bool autocorrect;
+  final bool enableSuggestions;
+  final InputBorder? border;
+  final InputBorder? focusedBorder;
+  final List<TextInputFormatter>? inputFormatters;
+  final Color focusedColor;
+  final Color enabledColor;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.hintText,
+    this.prefixIcon = Icons.person,
+    this.suffixIcon,
+    this.obscureText = false,
+    this.validator,
+    this.onChanged,
+    this.enabled = true,
+    this.keyboardType = TextInputType.text,
+    this.textInputAction,
+    this.autocorrect = true,
+    this.enableSuggestions = true,
+    this.border,
+    this.focusedBorder,
+    this.inputFormatters,
+    this.focusedColor = Colors.deepPurple,
+    this.enabledColor = Colors.deepPurple,
+  }) : super(key: key);
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 300,
+        child: TextFormField(
+
+          controller: widget.controller,
+          obscureText: widget.obscureText,
+          enabled: widget.enabled,
+          keyboardType: widget.keyboardType,
+          textInputAction: widget.textInputAction,
+          cursorColor: widget.focusedColor,
+          autocorrect: widget.autocorrect,
+          enableSuggestions: widget.enableSuggestions,
+          decoration: InputDecoration(
+            labelText: widget.labelText,
+            labelStyle: TextStyle(color: Colors.grey),
+            hintText: widget.hintText,
+            prefixIcon: Icon(widget.prefixIcon, color: Colors.deepPurple),
+            suffixIcon: widget.suffixIcon != null
+                ? Icon(widget.suffixIcon)
+                : null,
+            // border: OutlineInputBorder(
+            //   borderRadius: BorderRadius.circular(25.0),
+            //   borderSide: BorderSide(
+            //     color: Colors.blue,
+            //   ),
+            // ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+              borderSide: BorderSide(
+                color: widget.focusedColor,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(
+                style: BorderStyle.solid,
+                color: widget.enabledColor,
+              ),
+              gapPadding: 10,
+            ),
+          ),
+          validator: widget.validator,
+          onChanged: widget.onChanged,
+          inputFormatters: widget.inputFormatters,
+        )
+    );
+  }
+}
