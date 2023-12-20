3884c4c6edc4f19e3259c5373847d0e9ca616b46
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2023-12-20 23:36:08 +0100

    added new main and starting the backend process

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:trip_tales/src/pages/create_tale_page.dart';
 import 'package:trip_tales/src/pages/favorite_tales.dart';
 import 'package:trip_tales/src/pages/login.dart';
@@ -13,10 +16,12 @@ import 'package:trip_tales/src/widgets/menu_bar_tale.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );//.then((value) => Get.put(AuthenticationRepository));
   runApp(MyApp());
 }
 
